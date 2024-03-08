9c787bc0503756742c95e107b4fb83e4a9fbb49a
AliAkramkk <akramkorakkottil@gmail.com>
2024-03-08 17:31:31 +0530

    home page modified

--- src/pages/User/Home/Home.jsx
+++ src/pages/User/Home/Home.jsx
@@ -88,7 +88,7 @@ const Home = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 3000,
     responsive: [
       {
         breakpoint: 1024,
